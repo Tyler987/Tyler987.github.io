ea906cff595f779fa85dd51c6d0bff41e9edbd78
tyler <tylerkorth12@gmail.com>
2023-10-29 14:57:20 -0400

    pictures

--- assignments/prac6/script.js
+++ assignments/prac6/script.js
@@ -1,32 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // JSON data
-    const classesData = {
-      "workout_classes": [
-        // Your JSON data here...
-      ]
-    };
+const getClasses = async () => {
+    const url = "https://tyler987.github.io/assignments/prac6/class.json";
   
-    // Get the class container
-    const classContainer = document.getElementById("class-container");
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const createClassCard = (workoutClass) => {
+    const classCard = document.createElement("div");
+    classCard.classList.add("class-card"); // Define the CSS class for class cards in your styles.css.
   
-    // Loop through the classes and create HTML elements for each
-    classesData.workout_classes.forEach((workoutClass) => {
-      const classCard = document.createElement("div");
-      classCard.classList.add("class-card"); // You should define the CSS class for class cards in your styles.css.
+    const image = document.createElement("img");
+    image.src = workoutClass.Image;
+    image.classList.add("class-image");
   
-      // Create HTML structure for the class card
-      classCard.innerHTML = `
-        <h2>${workoutClass["Class Name"]}</h2>
-        <p>Class Type: ${workoutClass["Class Type"]}</p>
-        <p>Experience Level: ${workoutClass["Experience Level"]}</p>
-        <p>Exercise Type: ${workoutClass["Exercise Type"]}</p>
-        <p>Days Available: ${workoutClass["Days Available"].join(", ")}</p>
-        <p>Times Available: ${workoutClass["Times Available"].join(", ")}</p>
-        <p>Description: ${workoutClass["Description"]}</p>
-        <img src="${workoutClass["Image"]}" alt="${workoutClass["Class Name"]}" />
-      `;
+    const classDetails = document.createElement("div");
+    classDetails.classList.add("class-details");
   
-      // Append the class card to the class container
+    const title = document.createElement("h2");
+    title.textContent = workoutClass["Class Name"];
+    classDetails.appendChild(title);
+  
+    const classType = document.createElement("p");
+    classType.textContent = "Class Type: " + workoutClass["Class Type"];
+    classDetails.appendChild(classType);
+  
+    const experienceLevel = document.createElement("p");
+    experienceLevel.textContent = "Experience Level: " + workoutClass["Experience Level"];
+    classDetails.appendChild(experienceLevel);
+  
+    const exerciseType = document.createElement("p");
+    exerciseType.textContent = "Exercise Type: " + workoutClass["Exercise Type"];
+    classDetails.appendChild(exerciseType);
+  
+    const daysAvailable = document.createElement("p");
+    daysAvailable.textContent = "Days Available: " + workoutClass["Days Available"].join(", ");
+    classDetails.appendChild(daysAvailable);
+  
+    const timesAvailable = document.createElement("p");
+    timesAvailable.textContent = "Times Available: " + workoutClass["Times Available"].join(", ");
+    classDetails.appendChild(timesAvailable);
+  
+    const description = document.createElement("p");
+    description.textContent = "Description: " + workoutClass["Description"];
+    classDetails.appendChild(description);
+  
+    classCard.appendChild(image);
+    classCard.appendChild(classDetails);
+  
+    return classCard;
+  };
+  const showClasses = async () => {
+    let classes = await getClasses();
+    let classContainer = document.getElementById("class-container");
+  
+    classes.workout_classes.forEach((workoutClass) => {
+      const classCard = createClassCard(workoutClass);
       classContainer.appendChild(classCard);
     });
-  });
+  };
+  
+  // Show the workout classes
+  showClasses();
